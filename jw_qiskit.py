6748bbeccfbfea97510201554a13f3184df36601
Cheshta-Joshi <cheshta164@gmail.com>
2023-06-16 16:43:29 +0100

    Updated jw qiskit code

--- jw_qiskit.py
+++ jw_qiskit.py
@@ -3,12 +3,12 @@ import numpy as np
 from qiskit.opflow import (I, X,Y,Z,StateFn, Zero, One, Plus, Minus, H, CX,
                            DictStateFn, VectorStateFn, CircuitStateFn, OperatorStateFn)
 
-N = 3 
+N = 4 
 e = 1
 t = 2
 U = 3
 
-initial_state = Zero^N
+initial_state = One^N
 s_plus = (X - 1j*Y)/2
 s_minus = (X + 1j*Y)/2
 
@@ -19,6 +19,7 @@ x = r
 y = N-r-2
 full_op = (I^x) ^ op ^ (I^y) 
 
-print(full_op.eval())
+print(full_op.eval(initial_state))
+
 
 
