7526b25f391d74ae4ae8f6e75dfb8e60ae6c16c2
Cheshta-Joshi <cheshta164@gmail.com>
2023-06-16 16:40:18 +0100

    Added qiskit code for jordan wigner tranformed hamiltonian matrix

---
+++ jw_qiskit.py
@@ -0,0 +1,24 @@
+from qiskit import * 
+import numpy as np
+from qiskit.opflow import (I, X,Y,Z,StateFn, Zero, One, Plus, Minus, H, CX,
+                           DictStateFn, VectorStateFn, CircuitStateFn, OperatorStateFn)
+
+N = 3 
+e = 1
+t = 2
+U = 3
+
+initial_state = Zero^N
+s_plus = (X - 1j*Y)/2
+s_minus = (X + 1j*Y)/2
+
+r = 1
+op = e*((s_plus @ s_minus) ^ I) + U*((s_plus @ s_minus)^(s_plus @ s_plus)) + t *((s_plus ^ s_minus)+ (s_minus ^s_plus))
+
+x = r
+y = N-r-2
+full_op = (I^x) ^ op ^ (I^y) 
+
+print(full_op.eval())
+
+
